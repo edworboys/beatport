dbfadba276182039a513ee4f6e3699dd60110da8
Ed Worboys <edworboys@gmail.com>
2015-11-04 16:24:12 +0000

    changed package name, added requirements.txt parser and provided license

--- setup.py
+++ setup.py
@@ -2,17 +2,24 @@
 
 from setuptools import setup
 
+
+def parse_requirements(filename):
+    return list(filter(lambda line: (line.strip())[0] != '#',
+                      [line.strip() for line in open(filename).readlines()]))
+
+
+requirements = parse_requirements('requirements.txt')
+
 setup(
-        name='beatport',
+        name='beatport-api',
         version='0.0.1',
         description='Handles authentication for the Beatport API',
         url='https://github.com/jsakas/beatport',
         author='jsakas',
         author_email='jpsakas@gmail.com',
-        install_requires=[
-            'rauth','requests'
-            ],
+        license='MIT',
+        install_requires=requirements,
         py_modules=[
-            'beatport'
+            'beatport-api'
             ],
         )
